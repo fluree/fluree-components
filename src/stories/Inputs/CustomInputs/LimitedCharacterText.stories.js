35eeb62af35d5b2f02d249e1d0ec01ce28182922
Jonathan Dorety <jdorety@flur.ee>
2020-09-15 10:34:13 -0400

    Refactor to account for docs

--- src/stories/Inputs/CustomInputs/LimitedCharacterText.stories.js
+++ src/stories/Inputs/CustomInputs/LimitedCharacterText.stories.js
@@ -7,81 +7,204 @@ import { makeStyles } from "@material-ui/core/styles";
 import { alert } from "../../../themes/colors";
 
 export default {
-  title: "Inputs/Text/Custom/Limited Characters",
+  title: "Inputs/Custom/Text/Limited Characters",
 };
 
-const useStyles = makeStyles({
-  root: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "flex",
-    justifyContent: "flex-end",
-    fontFamily: "Open Sans",
-    fontSize: "0.75rem",
-    lineHeight: "6px",
-  },
-  overLimit: {
-    color: alert,
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     boxSizing: "border-box",
+//     width: "100%",
+//     display: "flex",
+//     justifyContent: "flex-end",
+//     fontFamily: "Open Sans",
+//     fontSize: "0.75rem",
+//     lineHeight: "6px",
+//   },
+//   overLimit: {
+//     color: alert,
+//   },
+// });
 
-const CharLimitHelper = (props) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      {props.children}
-      {props.length > props.maxLength ? (
-        <p className={classes.overLimit}>
-          {props.length}/{props.maxLength}
-        </p>
-      ) : (
-        <p>
-          {props.length}/{props.maxLength}
-        </p>
-      )}
-    </div>
-  );
-};
+// const CharLimitHelper = (props) => {
+//   const classes = useStyles();
+//   return (
+//     <div className={classes.root}>
+//       {props.children}
+//       {props.length > props.maxLength ? (
+//         <p className={classes.overLimit}>
+//           {props.length}/{props.maxLength}
+//         </p>
+//       ) : (
+//         <p>
+//           {props.length}/{props.maxLength}
+//         </p>
+//       )}
+//     </div>
+//   );
+// };
 
-const Template = (args) => {
+// const Template = (args) => {
+//   const [value, setValue] = useState("");
+
+//   const changeHandler = (e) => setValue(e.target.value);
+
+//   return (
+//     <FormControl variant="filled" error={value.length > args.maxLength}>
+//       <InputLabel htmlFor="example-input" variant="filled">
+//         {args.label}
+//       </InputLabel>
+//       <FilledInput
+//         id="example-input"
+//         aria-describedby="example-helper-text"
+//         onChange={changeHandler}
+//         value={value}
+//         multiline={args.multiline}
+//       />
+//       <FormHelperText
+//         id="example-helper-text"
+//         variant="filled"
+//         component={CharLimitHelper}
+//         length={value.length}
+//         maxLength={args.maxLength}
+//       >
+//         {args.helperText}
+//       </FormHelperText>
+//     </FormControl>
+//   );
+// };
+
+// export const SingleLine = Template.bind({});
+// SingleLine.args = {
+//   label: "Limited Characters",
+//   maxLength: 10,
+// };
+
+// export const MultiLineLimitedTextInput = Template.bind({});
+// MultiLineLimitedTextInput.args = {
+//   maxLength: 50,
+//   label: "Multi-line",
+//   multiline: true,
+// };
+
+export const SingleLine = () => {
+  const useStyles = makeStyles({
+    root: {
+      boxSizing: "border-box",
+      width: "100%",
+      display: "flex",
+      justifyContent: "flex-end",
+      fontFamily: "Open Sans",
+      fontSize: "0.75rem",
+      lineHeight: "6px",
+    },
+    overLimit: {
+      color: alert,
+    },
+  });
+
+  const CharLimitHelper = (props) => {
+    const classes = useStyles();
+    return (
+      <div className={classes.root}>
+        {props.children}
+        {props.length > props.maxLength ? (
+          <p className={classes.overLimit}>
+            {props.length}/{props.maxLength}
+          </p>
+        ) : (
+          <p>
+            {props.length}/{props.maxLength}
+          </p>
+        )}
+      </div>
+    );
+  };
+
   const [value, setValue] = useState("");
 
   const changeHandler = (e) => setValue(e.target.value);
 
   return (
-    <FormControl variant="filled" error={value.length > args.maxLength}>
+    <FormControl variant="filled" error={value.length > 10}>
       <InputLabel htmlFor="example-input" variant="filled">
-        {args.label}
+        Limited Characters
       </InputLabel>
       <FilledInput
         id="example-input"
         aria-describedby="example-helper-text"
         onChange={changeHandler}
         value={value}
-        multiline={args.multiline}
       />
       <FormHelperText
         id="example-helper-text"
         variant="filled"
         component={CharLimitHelper}
         length={value.length}
-        maxLength={args.maxLength}
+        maxLength={10}
       >
-        {args.helperText}
       </FormHelperText>
     </FormControl>
   );
 };
 
-export const SingleLine = Template.bind({});
-SingleLine.args = {
-  label: "Limited Characters",
-  maxLength: 10,
-};
+export const MultiLine = () => {
+  const useStyles = makeStyles({
+    root: {
+      boxSizing: "border-box",
+      width: "100%",
+      display: "flex",
+      justifyContent: "flex-end",
+      fontFamily: "Open Sans",
+      fontSize: "0.75rem",
+      lineHeight: "6px",
+    },
+    overLimit: {
+      color: alert,
+    },
+  });
 
-export const MultiLineLimitedTextInput = Template.bind({});
-MultiLineLimitedTextInput.args = {
-  maxLength: 50,
-  label: "Multi-line",
-  multiline: true,
-};
+  const CharLimitHelper = (props) => {
+    const classes = useStyles();
+    return (
+      <div className={classes.root}>
+        {props.children}
+        {props.length > props.maxLength ? (
+          <p className={classes.overLimit}>
+            {props.length}/{props.maxLength}
+          </p>
+        ) : (
+          <p>
+            {props.length}/{props.maxLength}
+          </p>
+        )}
+      </div>
+    );
+  };
+
+  const [value, setValue] = useState("");
+
+  const changeHandler = (e) => setValue(e.target.value);
+
+  return (
+    <FormControl variant="filled" error={value.length > 50}>
+      <InputLabel htmlFor="example-input" variant="filled">
+        Limited Characters
+      </InputLabel>
+      <FilledInput
+        id="example-input"
+        aria-describedby="example-helper-text"
+        onChange={changeHandler}
+        value={value}
+        multiline
+      />
+      <FormHelperText
+        id="example-helper-text"
+        variant="filled"
+        component={CharLimitHelper}
+        length={value.length}
+        maxLength={50}
+      >
+      </FormHelperText>
+    </FormControl>
+  );
+}
